76c47a08c80c0abc3ef329bf8c4c37f11eb96238
Matt Cuento <cuentomr17@gmail.com>
2020-05-19 14:43:20 -0700

    fixing errors

--- routes/users.js
+++ routes/users.js
@@ -11,7 +11,7 @@ router.get('/', function (req, res, next) {
 router.get('/all', userController.getAllUsers)
 
 // CREATE new user
-router.post('/new', userController.createUser)
+router.post('/new', userController.create_user)
 
 // DELETE a user by username
 router.delete('/delete/:username', userController.deleteUser)
